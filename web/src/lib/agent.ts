56ea8aea509c94dea58b26f51b44dacef1e8fdb2
Rahim <34522491+adoublef@users.noreply.github.com>
2023-02-06 01:22:50 +0000

    fix/mailing auth (#15)
    * add UI confirmation
    
    * using agent lib
    
    * created a lib directory
    
    * rm commits
    
    * fix for passwordless auth

---
+++ web/src/lib/agent.ts
@@ -0,0 +1,35 @@
+export const Auth = {
+    /** I should be able to return the response body of the request in a type-safe way */
+    verify: (token: string | null = "") => send("get", "/auth/v0/login", undefined, { Authorization: `Bearer ${token}` }),
+    /** I should be able to return the response body of the request in a type-safe way */
+    login: (email: string) => send("post", "/auth/v0/login", { email }),
+};
+
+/** 
+ * Does not throw an error, instead returns a 500 response
+ * My API should always return a json object, then can clean this up
+ */
+async function send(method: "post" | "get", url: string, payload?: any, headers: Record<string, string> = {}) {
+    let opts: RequestInit = { method, headers };
+
+    if (payload) {
+        headers["Content-Type"] = "application/json";
+        opts.body = JSON.stringify(payload);
+    }
+
+    try {
+        return fetch(import.meta.env.PUBLIC_API_URI + url, opts);
+    } catch (err) {
+        return new Response(null, { status: 500 });
+    }
+}
+
+async function parse<T extends Record<string, any>>(promise: Promise<Response>) {
+    const res = await promise;
+    switch (res.status) {
+        case 204:
+            return null;
+        default:
+            return res.json() as Promise<T>;
+    }
+}
