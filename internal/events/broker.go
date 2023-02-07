c8cda656cb3adda560a41283249ee8c27b1fe4dc
aboublef <kristopherab@gmail.com>
2023-02-07 21:57:47 +0000

    simplfied the events

--- internal/events/broker.go
+++ internal/events/broker.go
@@ -1,32 +1,22 @@
 package events
 
 import (
-	"bytes"
-	"encoding/gob"
 	"time"
 
 	"github.com/nats-io/nats.go"
 )
 
 type Broker interface {
-	Conn() *nats.Conn
-	Encode(v any) ([]byte, error)
-	Decode(p []byte, v any) error
-	Publish(subject string, data any) error
+	// Conn() *nats.Conn
 	Subscribe(subject string, cb nats.MsgHandler)
-	Request(subject string, data any, timeout time.Duration) ([]byte, error)
-	RequestAndResponse(subj string, response any, request any, timeout time.Duration) error
+	Publish(msg *nats.Msg) error
+	Request(msg *nats.Msg, timeout time.Duration) ([]byte, error)
 }
 
 var _ Broker = (*pubsub)(nil)
 
 type pubsub struct {
 	nc *nats.Conn
-}
-
-// Conn implements Broker
-func (p *pubsub) Conn() *nats.Conn {
-	return p.nc
 }
 
 func NewClient(nc *nats.Conn) Broker {
@@ -40,66 +30,15 @@ func (ps *pubsub) Subscribe(subject string, cb nats.MsgHandler) {
 	}
 }
 
-func (ps *pubsub) Publish(subject string, data any) error {
-	p, err := ps.Encode(data)
-	if err != nil {
-		return err
-	}
-
-	return ps.nc.Publish(subject, p)
+func (ps *pubsub) Publish(msg *nats.Msg) error {
+	return ps.nc.PublishMsg(msg)
 }
 
-func (ps *pubsub) Request(subject string, data any, timeout time.Duration) ([]byte, error) {
-	p, err := ps.Encode(data)
+func (ps *pubsub) Request(msg *nats.Msg, timeout time.Duration) ([]byte, error) {
+	m, err := ps.nc.RequestMsg(msg, timeout)
 	if err != nil {
 		return nil, err
 	}
 
-	msg, err := ps.nc.Request(subject, p, 5*time.Second)
-	if err != nil {
-		return nil, err
-	}
-
-	return msg.Data, nil
-}
-
-// TODO
-func (c *pubsub) RequestAndResponse(subj string, response, request any, timeout time.Duration) error {
-	q, err := Encode(request)
-	if err != nil {
-		return err
-	}
-
-	p, err := c.nc.Request(subj, q, timeout)
-	if err != nil {
-		return err
-	}
-
-	return Decode(p.Data, response)
-}
-
-func (*pubsub) Decode(p []byte, v any) error {
-	return gob.NewDecoder(bytes.NewReader(p)).Decode(v)
-}
-
-func (s *pubsub) Encode(v any) ([]byte, error) {
-	var buf bytes.Buffer
-	if err := gob.NewEncoder(&buf).Encode(v); err != nil {
-		return nil, err
-	}
-
-	return buf.Bytes(), nil
-}
-
-func Decode(p []byte, v any) error {
-	return gob.NewDecoder(bytes.NewReader(p)).Decode(v)
-}
-
-func Encode(v any) ([]byte, error) {
-	var buf bytes.Buffer
-	if err := gob.NewEncoder(&buf).Encode(v); err != nil {
-		return nil, err
-	}
-
-	return buf.Bytes(), nil
+	return m.Data, nil
 }
--- internal/events/events.go
+++ internal/events/events.go
@@ -1,21 +1,38 @@
 package events
 
-import "github.com/nats-io/nats.go"
+import (
+	"bytes"
+	"encoding/gob"
+
+	"github.com/nats-io/nats.go"
+)
+
+func Decode(p []byte, v any) error {
+	return gob.NewDecoder(bytes.NewReader(p)).Decode(v)
+}
+
+func Encode(v any) ([]byte, error) {
+	var buf bytes.Buffer
+	if err := gob.NewEncoder(&buf).Encode(v); err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
+}
 
 type Event string
 
 const (
-	EventLoginConfirmation   = "user.email.login"
-	EventSignupConfirmation  = "user.email.signup"
+	EventSendLoginConfirm    = "user.email.login"
+	EventSendSignupConfirm   = "user.email.signup"
 	EventGenerateLoginToken  = "token.generate.login" // NOTE not used
-	EventGenerateSignUpToken = "token.generate.signup"
-	// EventTokenVerify         = "token.verify"
+	EventGenerateSignupToken = "token.generate.signup"
+	EventVerifySignupToken   = "token.verify.signup"
 )
 
 // TODO implement Unmarshaler and Marshaler interfaces for binary encoding
 type DataSignUpConfirm struct {
-	Email    string
-	Username string
+	Email string
 }
 
 // TODO implement Unmarshaler and Marshaler interfaces for binary encoding
@@ -25,45 +42,40 @@ type DataLoginConfirm struct {
 }
 
 // TODO implement Unmarshaler and Marshaler interfaces for binary encoding
-type DataTokenGen struct {
+type DataEmailToken struct {
 	Token []byte
 }
 
-func NewSignupTokenMsg(token []byte) (*nats.Msg, error) {
-	v := DataTokenGen{Token: token}
+func EncodeSignupVerifyMsg(token []byte) (*nats.Msg, error) {
+	v := DataEmailToken{Token: token}
 	p, err := Encode(v)
 	if err != nil {
 		return nil, err
 	}
 	// Request from Auth service to get token from header.
-	msg := nats.Msg{
-		Subject: EventSignupConfirmation,
-		// token from header
-		Data: p,
-	}
-	return &msg, nil
+	return &nats.Msg{Subject: EventVerifySignupToken, Data: p}, nil
 }
 
-func NewSignupConfirmationMsg(email, username string) (*nats.Msg, error) {
+func EncodeSendSignupConfirmMsg(email string) (*nats.Msg, error) {
 	// send email to complete sign-up process
 	// automatically check which email provider so
 	// can send a link to the correct email provider
 	// https://www.freecodecamp.org/news/the-best-free-email-providers-2021-guide-to-online-email-account-services/
-	data := DataSignUpConfirm{Email: email, Username: username}
+	data := DataSignUpConfirm{Email: email}
 	p, err := Encode(data)
 	if err != nil {
 		return nil, err
 	}
 
-	return &nats.Msg{Subject: EventSignupConfirmation, Data: p}, nil
+	return &nats.Msg{Subject: EventSendSignupConfirm, Data: p}, nil
 }
 
-func NewLoginConfirmationMsg(email string, token []byte) (*nats.Msg, error) {
+func EncodeLoginConfirmMsg(email string, token []byte) (*nats.Msg, error) {
 	data := DataLoginConfirm{Email: email, Token: token}
 	p, err := Encode(data)
 	if err != nil {
 		return nil, err
 	}
 
-	return &nats.Msg{Subject: EventLoginConfirmation, Data: p}, nil
+	return &nats.Msg{Subject: EventSendLoginConfirm, Data: p}, nil
 }
