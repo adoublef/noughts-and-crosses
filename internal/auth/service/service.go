c3fc02aaa163c00b23adf61c0f66e2cb6b6690e0
aboublef <kristopherab@gmail.com>
2023-02-07 01:06:46 +0000

    auth service update

--- internal/auth/service/service.go
+++ internal/auth/service/service.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"time"
@@ -31,77 +32,116 @@ func New(nc *nats.Conn, tk jot.TokenClient) *Service {
 		e:  events.NewClient(nc),
 		tk: tk,
 	}
-	// go s.listen()
+	go s.listen()
 	s.routes()
 	return s
 }
 
 func (s *Service) routes() {
 	s.m.Post("/login", s.handleLogin())
-	s.m.Delete("/login", s.handleLogout())
+	// s.m.Delete("/login", s.handleLogout())
 	// should rename to `/login/verify` to
 	// avoid confusion or `/token/verify`
-	s.m.Get("/login", s.handleVerifyLogin())
+	s.m.Get("/login", s.handleConfirmLogin())
 
-	s.m.Get("/token", s.handleRefreshToken())
+	// s.m.Get("/token", s.handleRefreshToken())
 }
 
 func (s *Service) handleLogin() http.HandlerFunc {
-	type loginEmailReq struct {
+	type request struct {
 		Email string `json:"email"`
 	}
 
-	type restResp struct {
+	type response struct {
 		Message string `json:"message"`
 	}
 
+	newLoginTokenMsg := func(email string) (*nats.Msg, error) {
+		tk, err := s.tk.GenerateToken(context.Background(), jot.WithEnd(5*time.Minute), jot.WithPrivateClaims(jot.PrivateClaims{"email": email}))
+		if err != nil {
+			return nil, err
+		}
+
+		data := struct {
+			Email string
+			Token []byte
+		}{Email: email, Token: tk}
+		p, err := events.Encode(data)
+		if err != nil {
+			return nil, err
+		}
+
+		return &nats.Msg{Subject: events.EventUserLogin, Data: p}, nil
+	}
+
 	return func(w http.ResponseWriter, r *http.Request) {
-		var q loginEmailReq
+		var q request
 		if err := s.m.Decode(w, r, &q); err != nil {
 			s.m.Respond(w, r, err, http.StatusBadRequest)
 			return
 		}
 
-		// token creation request
-		// could be in a go-routine as its not super necessary to be sequential
-		go func() {
-			tk, err := s.tk.GenerateToken(context.Background(), jot.WithEnd(5*time.Minute), jot.WithPrivateClaims(jot.PrivateClaims{"email": q.Email}))
-			if err != nil {
-				// NOTE - send info to logging service
-				log.Println(err)
-				return
-			}
+		msg, err := newLoginTokenMsg(q.Email)
+		if err != nil {
+			s.m.Respond(w, r, err, http.StatusInternalServerError)
+			return
+		}
 
-			data := struct {
-				Email string
-				Token []byte
-			}{Email: q.Email, Token: tk}
-			p, err := events.Encode(data)
-			if err != nil {
-				// NOTE - send info to logging service
-				log.Println(err)
-				return
-			}
+		if err := s.e.Conn().PublishMsg(msg); err != nil {
+			s.m.Respond(w, r, err, http.StatusInternalServerError)
+			return
+		}
 
-			if err := s.e.Conn().PublishMsg(&nats.Msg{Subject: events.EventUserLogin, Data: p}); err != nil {
-				// NOTE - send info to logging service
-				log.Println(err)
-				return
-			}
-		}()
-
-		p := restResp{Message: "check emails for confirmation"}
-		s.m.Respond(w, r, p, http.StatusOK)
+		s.m.Respond(w, r, response{
+			Message: "check emails for confirmation",
+		}, http.StatusOK)
 	}
 }
 
-func (s *Service) handleVerifyLogin() http.HandlerFunc {
+func (s *Service) handleConfirmLogin() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		_, err := s.tk.ParseRequest(r)
+		tk, err := s.tk.ParseRequest(r)
 		if err != nil {
 			s.m.Respond(w, r, err, http.StatusUnauthorized)
 			return
 		}
+
+		// ask registry service if user exists
+		{
+			fmt.Println(tk.PrivateClaims()["email"].(string))
+
+			// email, _ := tk.PrivateClaims()["email"].(string)
+
+			// var (
+			// 	in  *nats.Msg
+			// 	out *nats.Msg
+			// 	err error
+			// )
+
+			// in = &nats.Msg{Subject: "profile.exists", Data: []byte(email)}
+			// out, err = s.e.Conn().RequestMsg(in, 5*time.Second)
+			// if err != nil {
+			// 	s.m.Respond(w, r, err, http.StatusInternalServerError)
+			// 	return
+			// }
+
+			// // pointer to struct
+			// var p *struct {
+			// 	ID uuid.UUID
+			// 	// use a defined type
+			// 	Email string
+			// 	// use a defined type
+			// 	Username string
+			// 	// use a defined type
+			// 	PhotoURL string
+			// }
+
+			// if err := events.Decode(out.Data, p); err != nil {
+			// 	s.m.Respond(w, r, err, http.StatusInternalServerError)
+			// 	return
+			// }
+		}
+		// create a session (using JWT)
 
 		s.m.Respond(w, r, nil, http.StatusOK)
 	}
@@ -116,5 +156,55 @@ func (s *Service) handleLogout() http.HandlerFunc {
 func (s *Service) handleRefreshToken() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		s.m.Respond(w, r, nil, http.StatusNotImplemented)
+	}
+}
+
+func (s *Service) listen() {
+	s.e.Subscribe(events.EventTokenGenerateSignUp, s.handleConfirmSignUp())
+}
+
+func (s *Service) handleConfirmSignUp() nats.MsgHandler {
+	// NOTE
+	newReplyMsg := func(subj string, email, username string) (*nats.Msg, error) {
+		encTk, err := s.tk.GenerateToken(context.Background(), jot.WithEnd(5*time.Minute), jot.WithPrivateClaims(jot.PrivateClaims{"email": email, "username": username}))
+		if err != nil {
+			return nil, err
+		}
+
+		// reply
+		tokenGen := struct {
+			Token []byte
+		}{Token: encTk}
+		p, err := events.Encode(tokenGen)
+		if err != nil {
+			return nil, err
+		}
+
+		return &nats.Msg{Subject: subj, Data: p}, nil
+	}
+
+	return func(msg *nats.Msg) {
+		var tokenBd struct {
+			Email    string
+			Username string
+		}
+
+		if err := events.Decode(msg.Data, &tokenBd); err != nil {
+			log.Println(err)
+			return
+		}
+
+		msg, err := newReplyMsg(msg.Reply, tokenBd.Email, tokenBd.Username)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		if err := s.e.Conn().PublishMsg(msg); err != nil {
+			log.Println(err)
+			return
+		}
+
+		log.Println("token generated")
 	}
 }
