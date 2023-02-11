1e2bc9c87f7b6866a30426809840888c69f70631
aboublef <kristopherab@gmail.com>
2023-02-11 13:38:34 +0000

    cleanup with component director

--- web/src/lib/agent.ts
+++ web/src/lib/agent.ts
@@ -1,3 +1,5 @@
+// https://web.dev/fetch-api-error-handling/
+
 export const User = {
     create: (token: string, email: string, username: string, bio?: string) => send<{ location: string; username: string; }>("post", "/registry/users", { email, username, bio }, { Authorization: `Bearer ${token}` }),
     signup: {
@@ -19,16 +21,25 @@ export const Ping = {
 } as const;
 
 /** 
- * Does not throw an error, instead returns a 500 response
- * My API should always return a json object, then can clean this up
+ * Does not throw an error, instead returns a 500 response.
+ * My API should always return a json object, then can clean this up.
  */
-async function send<T>(method: Method, url: string, payload?: unknown, headers: Record<string, string> = {}) {
+async function send<T>(method: Method, url: string, payload?: any, headers: Record<string, string> = {}): Promise<SendError | T> {
     let opts: RequestInit = { method, headers, mode: "cors" };
 
     if (payload) {
         headers["Content-Type"] = "application/json";
         opts.body = JSON.stringify(payload);
     }
+
+    // /*
+    const controller = new AbortController();
+    opts.signal = controller.signal;
+
+    // Cancel the fetch request in 1000ms
+    // NOTE: add a timeout to function arguments
+    setTimeout(() => controller.abort(), 1000);
+    // */
 
     try {
         const response = await fetch(import.meta.env.PUBLIC_API_URI + url, opts);
@@ -38,14 +49,42 @@ async function send<T>(method: Method, url: string, payload?: unknown, headers: 
         // 
         return (await response.json()) as T;
     } catch (err) {
+        // SyntaxError (?) JSON parsing error
+        if (err instanceof SyntaxError) {
+            // TODO handle syntax error
+            return { error: 1, name: "JSON parsing error" };
+        };
+
+        // DOMException: The user aborted a request. (AbortController)
+        if (err instanceof DOMException) {
+            // TODO handle syntax error
+            return { error: 2, name: "Request aborted" };
+        };
+
+        // SystemError: connect ECONNREFUSED
+        if (isSystemError(err)) {
+            // TODO handle syntax error
+            return { error: 3, name: "Connection refused" };
+        };
+
         const { cause } = (err as Error);
-        if (!isResponse(cause)) return { error: -1 } as const;
+        if (!isResponse(cause)) return { error: -1, name: "Unknown error" };
+
         switch (cause.status) {
             // TODO handle codes
             default:
-                return { error: cause.status } as const;
+                return { error: cause.status, name: cause.statusText };
         }
     }
+}
+
+type SendError = {
+    error: number;
+    name: string;
+};
+
+export function isSendError(err: any): err is SendError {
+    return err?.error !== undefined;
 }
 
 export function isResponse(err: any): err is Response {
