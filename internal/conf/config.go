f6b74ed5a416d2364036a98df962d92218f2462c
Kristopher Rahim <kristopherab@gmail.com>
2023-03-05 23:00:54 +0000

    cmd related

--- internal/conf/config.go
+++ internal/conf/config.go
@@ -24,14 +24,18 @@ func init() {
 	flag.IntVar(&PORT, "port", port, "port")
 
 	flag.StringVar(&ClientURI, "client-uri", os.Getenv("CLIENT_URI"), "client uri")
+
+	flag.StringVar(&DBURL, "database-uri", os.Getenv("DATABASE_URL"), "database uri")
+
+	flag.StringVar(&JWTSecret, "jwt-secret", os.Getenv("JWT_SECRET"), "jwt secret")
+
 	flag.StringVar(&SMTPHost, "smtp-host", os.Getenv("SMTP_HOST"), "smtp host")
 	flag.StringVar(&SMTPUsername, "smtp-username", os.Getenv("SMTP_EMAIL"), "smtp username")
 	flag.StringVar(&SMTPPassword, "smtp-password", os.Getenv("SMTP_PASS"), "smtp password")
-	flag.StringVar(&DBURL, "database-uri", os.Getenv("DATABASE_URL"), "database uri")
+
 	flag.StringVar(&NATSURI, "nats-uri", os.Getenv("NATS_URI"), "nats uri")
 	flag.StringVar(&NATSToken, "nats-token", os.Getenv("NATS_TOKEN"), "nats token")
 	flag.StringVar(&NATSSeed, "nats-seed", os.Getenv("NATS_SEED"), "nats seed")
-	flag.StringVar(&JWTSecret, "jwt-secret", os.Getenv("JWT_SECRET"), "jwt secret")
 
 	flag.Parse()
 }
